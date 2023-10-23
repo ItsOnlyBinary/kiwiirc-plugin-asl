3ed258a72bfe6d2a79aa274772d1f63fe0e3b72b
ItsOnlyBinary <git@itsonlybinary.com>
2023-10-23 21:13:21 +0100

    New webpack config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,47 @@
+const path = require('path');
+
 module.exports = {
     root: true,
+
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+
+    parser: 'vue-eslint-parser',
+
     parserOptions: {
         parser: '@babel/eslint-parser',
+        ecmaVersion: 2020,
+        extraFileExtensions: ['.vue'],
         sourceType: 'module',
     },
+
+    plugins: ['jsdoc'],
+
     extends: [
-        'plugin:vue/recommended',
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
         '@vue/airbnb',
         'standard',
     ],
-    env: {
-        browser: true,
+
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['@', path.join(__dirname, 'src')],
+                ],
+                extensions: ['.js', '.vue', '.json'],
+            },
+        },
     },
-    // required to lint *.vue files
-    plugins: [
-        'vue',
-    ],
-    // add your custom rules here
+
     rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+
         'class-methods-use-this': 0,
         'comma-dangle': ['error', {
             arrays: 'always-multiline',
@@ -27,39 +51,97 @@ module.exports = {
             functions: 'ignore',
         }],
         'import/extensions': 0,
-        'import/no-extraneous-dependencies': 0,
-        'import/no-unresolved': 0,
+        'import/no-cycle': 0,
+        'import/no-unresolved': [2, {
+            ignore: [
+                // These files will not exist if lint is run before the first build
+                '/res/locales/available\\.json$',
+                '/static/locales/\\S+\\.json$',
+            ],
+        }],
         'import/prefer-default-export': 0,
         'indent': ['error', 4],
+        // 'max-len': ['error', { code: 120 }],
+        'max-classes-per-file': 0,
         'no-continue': 0,
-        'no-control-regex': 0,
+        'no-else-return': 0,
         'no-multi-assign': 0,
         'no-param-reassign': ['error', { props: false }],
         'no-plusplus': 0,
         'no-prototype-builtins': 0,
-        'prefer-promise-reject-errors': 0,
-        'quote-props': ['error', 'consistent-as-needed'],
+        'no-control-regex': 0,
         'object-shorthand': 0,
         'operator-linebreak': 0,
         'prefer-const': 0,
         'prefer-destructuring': 0,
         'prefer-object-spread': 0,
+        'prefer-promise-reject-errors': 0,
         'prefer-template': 0,
+        'quote-props': ['error', 'consistent-as-needed'],
         'semi': ['error', 'always'],
+        'strict': 0,
         'space-before-function-paren': ['error', 'never'],
-        'vue/html-closing-bracket-spacing': 0,
+        'vue/component-definition-name-casing': 0,
         'vue/html-indent': ['error', 4],
+        // 'vue/max-len': [
+        //     'error',
+        //     {
+        //         code: 120,
+        //         template: 120,
+        //         tabWidth: 4,
+        //         comments: 120,
+        //     },
+        // ],
         'vue/max-attributes-per-line': 0,
-        'vue/singleline-html-element-content-newline': 0,
+        'vue/multi-word-component-names': 0,
         'vue/multiline-html-element-content-newline': 0,
+        'vue/no-mutating-props': 0,
+        'vue/no-unused-components': 0,
         'vue/no-v-html': 0,
+        'vue/one-component-per-file': 0,
+        'vue/require-default-prop': 0,
         'vue/require-prop-types': 0,
+        'vue/singleline-html-element-content-newline': 0,
         'vuejs-accessibility/anchor-has-content': 0,
         'vuejs-accessibility/click-events-have-key-events': 0,
         'vuejs-accessibility/form-control-has-label': 0,
         'vuejs-accessibility/iframe-has-title': 0,
         'vuejs-accessibility/interactive-supports-focus': 0,
         'vuejs-accessibility/label-has-for': 0,
+        'vuejs-accessibility/media-has-caption': 0,
         'vuejs-accessibility/mouse-events-have-key-events': 0,
+
+        // TODO vue3
+        'multiline-ternary': 0,
+        'vue/operator-linebreak': 0,
+        'vue/comma-dangle': 0,
+        'vue/space-infix-ops': 0,
+        'vue/object-curly-newline': 0,
+        'vue/object-curly-spacing': 0,
+        'vue/key-spacing': 0,
+        'vue/no-template-target-blank': 0,
+        'vue/no-setup-props-destructure': 0,
+        'vue/no-v-for-template-key': 0,
+        'vue/quote-props': 0,
+        'vue/require-explicit-emits': 0,
+        'vue/v-on-event-hyphenation': 0,
     },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+        {
+            files: ['webpack.config.js', 'build/**/*.js'],
+            rules: {
+                'import/no-extraneous-dependencies': 0,
+                'no-console': 0,
+            },
+        },
+    ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,130 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const makeSourceMap = process.argv.indexOf('--srcmap') > -1;
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/plugin.js',
-    output: {
-        filename: 'plugin-asl.js',
-    },
-    externals: {
-        'vue': 'kiwi.Vue',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
+const outputFileName = 'plugin-asl.js';
+
+module.exports = (env, argv) => {
+    const isDev = env.WEBPACK_SERVE;
+
+    const options = {
+        mode: isDev ? 'development' : 'production',
+        sourceMap: undefined,
+        extraPlugins: []
+    };
+
+    if (argv.mode) {
+        options.mode = argv.mode;
+    }
+
+    if (options.mode === 'development') {
+        options.sourceMap = env.WEBPACK_SERVE ? 'eval-source-map' : 'source-map';
+    } else if (env.srcmap) {
+        options.sourceMap = 'source-map';
+    }
+
+    return {
+        context: process.cwd(),
+        mode: options.mode,
+        entry: {
+            plugin: './src/plugin.js',
+        },
+        output: {
+            path: path.join(__dirname, 'dist'),
+            publicPath: 'auto',
+            filename: outputFileName,
+            clean: true,
+        },
+        resolve: {
+            alias: {
+                '@': path.join(__dirname, 'src'),
             },
-            {
-                test: /\.js$/,
-                use: [{loader: 'babel-loader'}],
-                include: [
-                    path.join(__dirname, 'src'),
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            },
-            {
-                test: /\.less$/,
-                use: [ 'vue-style-loader', 'css-loader', 'less-loader' ]
-            }
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
+            extensions: ['.js', '.jsx', '.vue', '.json'],
+        },
+        externals: {
+            'vue': 'kiwi.Vue',
+        },
+        module: {
+            rules: [
                 {
-                    from: path.resolve(__dirname, './res/locales'),
-                    to: 'plugin-asl/locales/',
-                    toType: 'dir',
-                    globOptions: {
-                        ignore: ['.*']
-                    },
-                }
-            ],
-        })
-    ],
-    devtool: makeSourceMap ? 'source-map' : undefined,
-    devServer: {
-        static: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
+                    test: /\.vue$/,
+                    use: ['vue-loader'],
+                },
+                {
+                    test: /\.js$/,
+                    use: ['thread-loader', 'babel-loader'],
+                },
+                {
+                    test: /\.css$/,
+                    use: styleUse(),
+                },
+                {
+                    test: /\.less$/,
+                    use: styleUse('less-loader'),
+                },
+                {
+                    test: /\.s[ac]ss$/,
+                    use: styleUse('sass-loader'),
+                },
+            ]
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, './res/locales'),
+                        to: 'plugin-asl/locales/',
+                        toType: 'dir',
+                        globOptions: {
+                            ignore: ['.*']
+                        },
+                    }
+                ],
+            }),
+            ...options.extraPlugins,
+        ],
+        devtool: options.sourceMap,
+        devServer: {
+            static: path.join(__dirname, "dist"),
+            compress: true,
+            port: 9000,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
         }
     }
 };
+
+function styleUse(loader) {
+    const use = [
+        { loader: 'thread-loader' },
+        { loader: 'style-loader' },
+        {
+            loader: 'css-loader',
+            options: {
+                importLoaders: 2,
+                esModule: false,
+            },
+        },
+        {
+            loader: 'postcss-loader',
+            options: {
+                postcssOptions: {
+                    plugins: [
+                        autoprefixer,
+                        cssnano,
+                    ],
+                },
+            },
+        },
+    ];
+
+    if (loader) {
+        use.push({ loader });
+    }
+
+    return use;
+}
